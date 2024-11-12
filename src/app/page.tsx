9b8bf6a65ae6e3d173f0e5eecfc3adf666cf7faf
jacob.andersson <jacob.andersson@se360.se>
2024-11-12 16:45:04 +0100

    m

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 
+
 export default function Home() {
   return (
-    <div>
-      COMBOOOOO
+    <div className="comboscreen">
+      <img style={{
+        width: '100%', height: '100%', objectFit: 'contain'
+      }} src="/Combo_Logo.svg" />
     </div>
   );
 }
